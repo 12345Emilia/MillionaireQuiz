2528940399ae648bed34dd687efba25d6aff55a5
Emilia <emilia.assarsson@hotmail.com>
2021-09-07 21:40:22 -0600

    Updated questions

--- src/App.js
+++ src/App.js
@@ -13,28 +13,6 @@ function App() {
   const data = [
     {
       id: 1,
-      question: "What's the name of Americas President?",
-      answers: [
-        {
-          text: 'Joe',
-          correct: true,
-        },
-        {
-          text: 'Donald',
-          correct: false,
-        },
-        {
-          text: 'Tom',
-          correct: false,
-        },
-        {
-          text: 'Michael',
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 2,
       question: 'Which color is not on the American flag?',
       answers: [
         {
@@ -56,7 +34,7 @@ function App() {
       ],
     },
     {
-      id: 3,
+      id: 2,
       question: "What's the capital of the United States?",
       answers: [
         {
@@ -73,6 +51,293 @@ function App() {
         },
         {
           text: 'Washington D.C',
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "What's not a right in the Declaration of Independence?",
+      answers: [
+        {
+          text: 'Life',
+          correct: false,
+        },
+        {
+          text: 'Love',
+          correct: true,
+        },
+        {
+          text: 'Liberty',
+          correct: false,
+        },
+        {
+          text: 'Pursuit of Happiness',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: 'Which weekend is the start of summer?',
+      answers: [
+        {
+          text: 'Labour Day Weekend',
+          correct: false,
+        },
+        {
+          text: '4th of July Weekend',
+          correct: false,
+        },
+        {
+          text: 'First weekend of June',
+          correct: false,
+        },
+        {
+          text: 'Memorial Day Weekend',
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: 'Which city is also known as the big apple?',
+      answers: [
+        {
+          text: 'Los Angeles',
+          correct: false,
+        },
+        {
+          text: 'Washington D.C.',
+          correct: false,
+        },
+        {
+          text: 'New York',
+          correct: true,
+        },
+        {
+          text: 'Miami',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: 'When is Black Friday?',
+      answers: [
+        {
+          text: 'Thursday following Thanksgiving Day',
+          correct: false,
+        },
+        {
+          text: 'Friday following Thanksgiving Day',
+          correct: true,
+        },
+        {
+          text: 'Friday before Thanksgiving Day',
+          correct: false,
+        },
+        {
+          text: 'Friday following Halloween',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: 'When was the Declaration of Independence adopted?',
+      answers: [
+        {
+          text: 'July 4th, 1776',
+          correct: true,
+        },
+        {
+          text: 'July 24th, 1777',
+          correct: false,
+        },
+        {
+          text: 'July 14th, 1778',
+          correct: false,
+        },
+        {
+          text: 'July 4th, 1775',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "What's the population of America?",
+      answers: [
+        {
+          text: '333,300,000',
+          correct: true,
+        },
+        {
+          text: '400,200,000',
+          correct: false,
+        },
+        {
+          text: '280,200,000',
+          correct: false,
+        },
+        {
+          text: '333,999,000',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: 'Who was Americas first President?',
+      answers: [
+        {
+          text: 'Franklin Roosevelt',
+          correct: false,
+        },
+        {
+          text: 'Donald Trump',
+          correct: false,
+        },
+        {
+          text: 'Benjamin Franklin',
+          correct: false,
+        },
+        {
+          text: 'George Washington',
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: 'How many amendments does the Constitution have?',
+      answers: [
+        {
+          text: '21',
+          correct: false,
+        },
+        {
+          text: '25',
+          correct: false,
+        },
+        {
+          text: '27',
+          correct: true,
+        },
+        {
+          text: '29',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: 'Which one is not one of the 13 original states?',
+      answers: [
+        {
+          text: 'New York',
+          correct: false,
+        },
+        {
+          text: 'New Orleans',
+          correct: true,
+        },
+        {
+          text: 'New Hampshire',
+          correct: false,
+        },
+        {
+          text: 'New Jersey',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: 'What ocean is on the west coast of the United States?',
+      answers: [
+        {
+          text: 'Pacific Ocean',
+          correct: true,
+        },
+        {
+          text: 'Atlantic Ocean',
+          correct: false,
+        },
+        {
+          text: 'Black Sea',
+          correct: false,
+        },
+        {
+          text: 'Gulf of Mexico',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: 'What season is pumpkin spice associated with?',
+      answers: [
+        {
+          text: 'Summer',
+          correct: false,
+        },
+        {
+          text: 'Winter',
+          correct: false,
+        },
+        {
+          text: 'Autumn',
+          correct: true,
+        },
+        {
+          text: 'Spring',
+          correct: false,
+        },
+      ],
+    },
+
+    {
+      id: 14,
+      question: 'How many states are there in America?',
+      answers: [
+        {
+          text: '50',
+          correct: true,
+        },
+        {
+          text: '49',
+          correct: false,
+        },
+        {
+          text: '52',
+          correct: false,
+        },
+        {
+          text: '51',
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "What's often referred to as America's National Pastime?",
+      answers: [
+        {
+          text: 'American Football',
+          correct: false,
+        },
+        {
+          text: 'Idol',
+          correct: false,
+        },
+        {
+          text: 'Dancing with the Stars',
+          correct: false,
+        },
+        {
+          text: 'Baseball',
           correct: true,
         },
       ],
@@ -110,8 +375,6 @@ function App() {
 
   return (
     <div className='app'>
-      <h1 className='title'>Welcome to Americas Game Show</h1>
-      <h3 className='title2'>Who Wants to Be a Millionaire</h3>
       {username ? (
         <>
           <div className='main'>
